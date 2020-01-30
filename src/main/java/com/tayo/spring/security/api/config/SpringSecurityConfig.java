e69fec7ba63d699447517aea2fad4c7ec37087f3
Talabi Omotayo <you@example.com>
2020-01-30 13:28:26 +0100

    spring security push

---
+++ src/main/java/com/tayo/spring/security/api/config/SpringSecurityConfig.java
@@ -0,0 +1,54 @@
+package com.tayo.spring.security.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+
+        auth.inMemoryAuthentication().withUser("tayo").password("Password").roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("tobi").password("Password2").roles("USER");
+    }
+
+
+
+    // security for all API
+
+    /*
+     * @Override protected void configure(HttpSecurity http) throws Exception {
+     * http.csrf().disable();
+     * http.authorizeRequests().anyRequest().fullyAuthenticated().and().
+     * httpBasic(); }
+     */
+
+    // security based on URL
+
+    /*
+     * @Override protected void configure(HttpSecurity http) throws Exception {
+     * http.csrf().disable();
+     * http.authorizeRequests().antMatchers("/rest/**").fullyAuthenticated().and
+     * ().httpBasic(); }
+     */
+
+    // security based on ROLE
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests().antMatchers("/rest/**").hasAnyRole("ADMIN").anyRequest().fullyAuthenticated().and()
+                .httpBasic();
+    }
+
+    @Bean
+    public static NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
+}
+
+
---
+++ src/main/java/com/tayo/spring/security/api/controller/NoAuthController.java
@@ -0,0 +1,19 @@
+package com.tayo.spring.security.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/noAuth/rest")
+public class NoAuthController {
+
+    @GetMapping("/sayHello")
+    public String sayHi() {
+        return "hello";
+    }
+
+}
+
+
